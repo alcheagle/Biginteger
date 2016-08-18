30ae3b8e7da238fcf20f8fbcc83b3f6c6861a7df
Andrea Gilardoni <andrea.gilardoni96@gmail.com>
2016-08-18 09:27:16 +0000

    created constructor with byte array and bug fixing

--- include/bigint.hpp
+++ include/bigint.hpp
@@ -7,122 +7,124 @@
 
 #define UBIGGEST_TYPE unsigned long long 
 
-namespace bigint{
-	class UnsignedBigint{
-	private:
-		std::vector<UBIGGEST_TYPE> number;
-	public:
-		//Constructors
-		UnsignedBigint();
-		UnsignedBigint(const UBIGGEST_TYPE&);
-		UnsignedBigint(const std::string &representation);
-		UnsignedBigint(const std::string &representation, unsigned int  base);
-		UnsignedBigint(const UnsignedBigint& b);
-		UnsignedBigint(unsigned char* bytes, unsigned int length); //FIXME endianness problems
+namespace bigint {
 
-		//Adding
-		UnsignedBigint sum(const UnsignedBigint&) const;
-		UnsignedBigint &selfSum(const UnsignedBigint&);
-		UnsignedBigint sum(const UBIGGEST_TYPE&) const;
-		UnsignedBigint &selfSum(const UBIGGEST_TYPE&);
-		UnsignedBigint operator+(const UnsignedBigint&) const;
-		UnsignedBigint &operator+=(const UnsignedBigint&);
-		UnsignedBigint operator+(const UBIGGEST_TYPE&) const;
-		UnsignedBigint &operator+=(const UBIGGEST_TYPE&);
+    class UnsignedBigint {
+    private:
+	std::vector<UBIGGEST_TYPE> number;
+    public:
+	//Constructors
+	UnsignedBigint();
+	UnsignedBigint(const UBIGGEST_TYPE&);
+	UnsignedBigint(const std::string &representation);
+	UnsignedBigint(const std::string &representation, unsigned int base);
+	UnsignedBigint(const UnsignedBigint& b);
+	UnsignedBigint(unsigned char* bytes, unsigned int length); //FIXME endianness problems
 
-		//Subtraction
-		UnsignedBigint sub(const UnsignedBigint&) const;
-		UnsignedBigint &selfSub(const UnsignedBigint&);
-		UnsignedBigint sub(const UBIGGEST_TYPE&) const;
-		UnsignedBigint &selfSub(const UBIGGEST_TYPE&);
-		UnsignedBigint operator-(UnsignedBigint const &) const;
-		UnsignedBigint &operator-=(UnsignedBigint const &);
-		UnsignedBigint operator-(const UBIGGEST_TYPE&) const;
-		UnsignedBigint &operator-=(const UBIGGEST_TYPE&);
+	//Adding
+	UnsignedBigint sum(const UnsignedBigint&) const;
+	UnsignedBigint &selfSum(const UnsignedBigint&);
+	UnsignedBigint sum(const UBIGGEST_TYPE&) const;
+	UnsignedBigint &selfSum(const UBIGGEST_TYPE&);
+	UnsignedBigint operator+(const UnsignedBigint&) const;
+	UnsignedBigint &operator+=(const UnsignedBigint&);
+	UnsignedBigint operator+(const UBIGGEST_TYPE&) const;
+	UnsignedBigint &operator+=(const UBIGGEST_TYPE&);
 
-		//Multiplication
-		UnsignedBigint mul(const UnsignedBigint &);
-		UnsignedBigint &selfMul(const UnsignedBigint &);
-		UnsignedBigint mul(const UBIGGEST_TYPE&);
-		UnsignedBigint &selfMul(const UBIGGEST_TYPE&);
-		UnsignedBigint operator*(UnsignedBigint const &);
-		UnsignedBigint &operator*=(UnsignedBigint const &);
-		UnsignedBigint operator*(long long const &);
-		UnsignedBigint &operator*=(int const &);
+	//Subtraction
+	UnsignedBigint sub(const UnsignedBigint&) const;
+	UnsignedBigint &selfSub(const UnsignedBigint&);
+	UnsignedBigint sub(const UBIGGEST_TYPE&) const;
+	UnsignedBigint &selfSub(const UBIGGEST_TYPE&);
+	UnsignedBigint operator-(UnsignedBigint const &) const;
+	UnsignedBigint &operator-=(UnsignedBigint const &);
+	UnsignedBigint operator-(const UBIGGEST_TYPE&) const;
+	UnsignedBigint &operator-=(const UBIGGEST_TYPE&);
 
-		//division
-		UnsignedBigint div(const UnsignedBigint &);
-        UnsignedBigint &selfDiv(const UnsignedBigint &);
-        UnsignedBigint div(const UBIGGEST_TYPE&);
-        UnsignedBigint &selfDiv(const UBIGGEST_TYPE&);
-        UnsignedBigint operator/(UnsignedBigint const &);
-        UnsignedBigint &operator/=(UnsignedBigint const &);
-        UnsignedBigint operator/(long long const &);
-        UnsignedBigint &operator/=(int const &);
+	//Multiplication
+	UnsignedBigint mul(const UnsignedBigint &);
+	UnsignedBigint &selfMul(const UnsignedBigint &);
+	UnsignedBigint mul(const UBIGGEST_TYPE&);
+	UnsignedBigint &selfMul(const UBIGGEST_TYPE&);
+	UnsignedBigint operator*(UnsignedBigint const &);
+	UnsignedBigint &operator*=(UnsignedBigint const &);
+	UnsignedBigint operator*(long long const &);
+	UnsignedBigint &operator*=(int const &);
 
-		//module
-		UnsignedBigint mod(const UnsignedBigint &);
-        UnsignedBigint &selfMod(const UnsignedBigint &);
-        UnsignedBigint mod(const UBIGGEST_TYPE&);
-        UnsignedBigint &selfMod(const UBIGGEST_TYPE&);
-        UnsignedBigint operator%(UnsignedBigint const &);
-        UnsignedBigint &operator%=(UnsignedBigint const &);
-        UnsignedBigint operator%(long long const &);
-        UnsignedBigint &operator%=(int const &);
+	//division
+	UnsignedBigint div(const UnsignedBigint &);
+	UnsignedBigint &selfDiv(const UnsignedBigint &);
+	UnsignedBigint div(const UBIGGEST_TYPE&);
+	UnsignedBigint &selfDiv(const UBIGGEST_TYPE&);
+	UnsignedBigint operator/(UnsignedBigint const &);
+	UnsignedBigint &operator/=(UnsignedBigint const &);
+	UnsignedBigint operator/(long long const &);
+	UnsignedBigint &operator/=(int const &);
 
-		//Compare
-		int compare(UnsignedBigint const &) const; //0 a == b, -1 a < b, 1 a > b
-		bool operator<(const UnsignedBigint &) const;
-		bool operator>(const UnsignedBigint &) const;
-		bool operator<=(const UnsignedBigint &) const;
-		bool operator>=(const UnsignedBigint &) const;
-		bool operator==(const UnsignedBigint &) const;
+	//module
+	UnsignedBigint mod(const UnsignedBigint &);
+	UnsignedBigint &selfMod(const UnsignedBigint &);
+	UnsignedBigint mod(const UBIGGEST_TYPE&);
+	UnsignedBigint &selfMod(const UBIGGEST_TYPE&);
+	UnsignedBigint operator%(UnsignedBigint const &);
+	UnsignedBigint &operator%=(UnsignedBigint const &);
+	UnsignedBigint operator%(long long const &);
+	UnsignedBigint &operator%=(int const &);
 
-		//Allocation
-		UnsignedBigint operator=(const UBIGGEST_TYPE&);
+	//Compare
+	int compare(UnsignedBigint const &) const; //0 a == b, -1 a < b, 1 a > b
+	bool operator<(const UnsignedBigint &) const;
+	bool operator>(const UnsignedBigint &) const;
+	bool operator<=(const UnsignedBigint &) const;
+	bool operator>=(const UnsignedBigint &) const;
+	bool operator==(const UnsignedBigint &) const;
 
-		//Access
-		int operator[](const int &);
+	//Allocation
+	UnsignedBigint operator=(const UBIGGEST_TYPE&);
 
-		//Input&Output
-		friend std::istream &operator>>(std::istream &, UnsignedBigint &);
-		friend std::ostream &operator<<(std::ostream &, const UnsignedBigint&);
+	//Access
+	int operator[](const int &);
 
-		//Helpers
-		void clear();
+	//Input&Output
+	friend std::istream &operator>>(std::istream &, UnsignedBigint &);
+	friend std::ostream &operator<<(std::ostream &, const UnsignedBigint&);
 
-		//Power
-		UnsignedBigint &pow(int const &);
+	//Helpers
+	void clear();
 
-		//Trivia
-		int digits() const;
-		int trailing_zeros() const;
+	//Power
+	UnsignedBigint &pow(int const &);
 
-		//bitwise operators
-		UnsignedBigint andb(const UnsignedBigint &) const;
-		UnsignedBigint orb(const UnsignedBigint &) const;
-		UnsignedBigint notb() const;
-		UnsignedBigint xorb(const UnsignedBigint &) const;
-		UnsignedBigint shiftLeft(const UnsignedBigint &) const;
-		UnsignedBigint shiftRight(const UnsignedBigint &) const;
-		UnsignedBigint shiftLeft(const UBIGGEST_TYPE&) const;
-		UnsignedBigint shiftRight(const UBIGGEST_TYPE&) const;
-		UnsignedBigint operator&(const UnsignedBigint &) const;
-		UnsignedBigint operator|(const UnsignedBigint &) const;
-		UnsignedBigint operator~() const;
-		UnsignedBigint operator^(const UnsignedBigint &) const;
-		UnsignedBigint operator<<(const UnsignedBigint &) const;
-		UnsignedBigint operator>>(const UnsignedBigint &) const;
-		UnsignedBigint operator<<(const UBIGGEST_TYPE&) const;
-		UnsignedBigint operator>>(const UBIGGEST_TYPE&) const;
+	//Trivia
+	int digits() const;
+	int trailing_zeros() const;
 
-		//size
-		unsigned int size() const;
-		
-		std::string to_string() const;
-	private:
-		void init_with_base(const std::string& representation, unsigned int base);
-	};
+	//bitwise operators
+	UnsignedBigint andb(const UnsignedBigint &) const;
+	UnsignedBigint orb(const UnsignedBigint &) const;
+	UnsignedBigint notb() const;
+	UnsignedBigint xorb(const UnsignedBigint &) const;
+	UnsignedBigint shiftLeft(const UnsignedBigint &) const;
+	UnsignedBigint shiftRight(const UnsignedBigint &) const;
+	UnsignedBigint shiftLeft(const UBIGGEST_TYPE&) const;
+	UnsignedBigint shiftRight(const UBIGGEST_TYPE&) const;
+	UnsignedBigint operator&(const UnsignedBigint &) const;
+	UnsignedBigint operator|(const UnsignedBigint &) const;
+	UnsignedBigint operator~() const;
+	UnsignedBigint operator^(const UnsignedBigint &) const;
+	UnsignedBigint operator<<(const UnsignedBigint &) const;
+	UnsignedBigint operator>>(const UnsignedBigint &) const;
+	UnsignedBigint operator<<(const UBIGGEST_TYPE&) const;
+	UnsignedBigint operator>>(const UBIGGEST_TYPE&) const;
+
+	//size
+	unsigned int size() const;
+
+	std::string to_string() const;
+	std::string pretty_to_string() const;
+    private:
+	void init_with_base(const std::string& representation, unsigned int base);
+    };
 }
 
 #endif
--- src/bigint.cpp
+++ src/bigint.cpp
@@ -4,103 +4,138 @@
 #include <boost/regex.hpp>
 #include <iomanip>
 #include <boost/assert.hpp>
+#include <cmath>
 
 namespace bigint {
-	static const boost::regex number_regex1("(0x)([0-9a-fA-F]{1,})|(0)([0-7]{1,})|([0-9]{1,})");
-	static const boost::regex hex_number_regex("0x([0-9a-fA-F]{1,})");
-	static const boost::regex oct_number_regex("0([0-7]{1,})");
-	static const boost::regex dec_number_regex("([0-9]{1,})");
-	static const boost::regex number_regex2("([0-9]{1,})"); //FIXME for safer use a regex which checks also the interval of the value
+    //static const boost::regex number_regex1("(0x)([0-9a-fA-F]{1,})|(0)([0-7]{1,})|([0-9]{1,})");
+    static const boost::regex hex_number_regex("0x([0-9a-fA-F]{1,})");
+    static const boost::regex oct_number_regex("0([0-7]{1,})");
+    static const boost::regex dec_number_regex("([0-9]{1,})");
+    //static const boost::regex number_regex2("([0-9]{1,})"); //FIXME for safer use a regex which checks also the interval of the value
 
-	static int is_big_endian(void) {
+    static int is_big_endian(void) {
 
-		union {
-			uint32_t i;
-			char c[4];
-		} bint = {0x01020304};
+	union {
+	    uint32_t i;
+	    char c[4];
+	} bint = {0x01020304};
 
-		return bint.c[0] == 1;
+	return bint.c[0] == 1;
+    }
+
+    static UBIGGEST_TYPE ipow(UBIGGEST_TYPE base, UBIGGEST_TYPE exp) {
+	UBIGGEST_TYPE result = 1;
+	while (exp) {
+	    if (exp & 1)
+		result *= base;
+	    exp >>= 1;
+	    base *= base;
 	}
 
-	//Constructor
+	return result;
+    }
 
-	UnsignedBigint::UnsignedBigint()
-	: number() {
-		number.push_back(0);
+    //Constructor
+
+    UnsignedBigint::UnsignedBigint()
+    : number() {
+	number.push_back(0);
+    }
+
+    UnsignedBigint::UnsignedBigint(const UnsignedBigint &b)
+    : number(b.number) {
+    }
+
+    UnsignedBigint::UnsignedBigint(const UBIGGEST_TYPE& value) {
+	number.push_back(value);
+    }
+
+    UnsignedBigint::UnsignedBigint(const std::string &representation) {
+	boost::smatch matches;
+	std::string *number = NULL;
+	unsigned int base = 0;
+
+	if (boost::regex_match(representation, matches, hex_number_regex)) {
+	    number = new std::string(matches[1].first, matches[1].second);
+	    base = 16;
+	} else if (boost::regex_match(representation, matches, oct_number_regex)) {
+	    number = new std::string(matches[1].first, matches[1].second);
+	    base = 8;
+	} else if (boost::regex_match(representation, matches, dec_number_regex)) {
+	    number = new std::string(matches[1].first, matches[1].second);
+	    base = 10;
+	} else {
+	    BOOST_ASSERT_MSG(true, "not recognized number represetation"); ///TODO throw exception instead
 	}
 
-	UnsignedBigint::UnsignedBigint(const UnsignedBigint &b)
-	: number(b.number) {
-	}
+	std::cout << "match: " << *number << "base:" << base << std::endl;
+	init_with_base(*number, base);
+	delete number;
+    }
 
-	UnsignedBigint::UnsignedBigint(const UBIGGEST_TYPE& value) {
+    UnsignedBigint::UnsignedBigint(const std::string &representation, unsigned int base) {
+	init_with_base(representation, base);
+    }
+
+    UnsignedBigint::UnsignedBigint(unsigned char* bytes, unsigned int length) {
+	UBIGGEST_TYPE value = 0;
+	for (unsigned int i = 0; i < length; i++) {
+	    UBIGGEST_TYPE next = ((UBIGGEST_TYPE) bytes[i]) << (((length - 1 - i) % sizeof (UBIGGEST_TYPE))*8);
+	    value = value | next;
+//	    std::cout << "module: " << (i % sizeof(UBIGGEST_TYPE)) << std::endl;
+//	    std::cout << std::setfill(' ') << std::setw(10) << "char: 0x" << std::hex << (unsigned int)bytes[i] << std::endl;
+//	    std::cout << std::setfill(' ') << std::setw(10) << "next: 0x" << std::hex << next << std::endl;
+//	    std::cout << std::setfill(' ') << std::setw(10) << "value: 0x" << std::hex << value << std::endl;
+
+	    if (((i + 1) % sizeof (UBIGGEST_TYPE)) == 0 || (i + 1) == length) {
+		//		std::cout << "storing: 0x" << std::hex << value << std::endl;
 		number.push_back(value);
+		value = 0;
+	    }
 	}
+    }
 
-	UnsignedBigint::UnsignedBigint(const std::string &representation) {
-		boost::smatch matches;
-		std::string *number = NULL;
-		unsigned int base = 0;
+    void UnsignedBigint::init_with_base(const std::string& representation, unsigned int base) {
+	//TODO decide how representation should be
+	//static unsigned long long upper_boound = std::pow(2, sizeof(UBIGGEST_TYPE));
 
-		if (boost::regex_match(representation, matches, hex_number_regex)) {
-			number = new std::string(matches[1].first, matches[1].second);
-			base = 16;
-		} else if (boost::regex_match(representation, matches, oct_number_regex)) {
-			number = new std::string(matches[1].first, matches[1].second);
-			base = 8;
-		} else if (boost::regex_match(representation, matches, dec_number_regex)) {
-			number = new std::string(matches[1].first, matches[1].second);
-			base = 10;
-		} else {
-			BOOST_ASSERT_MSG(true, "not recognized number represetation");///TODO throw exception instead
-		}
-		
-		std::cout << "match: " << *number << "base:" << base << std::endl;
-		init_with_base(*number, base);
-		delete number;
+	number.push_back(0);
+    }
+
+    std::string UnsignedBigint::to_string() const {
+	std::stringstream ss;
+	ss << *this;
+	return ss.str();
+    }
+
+    std::string UnsignedBigint::pretty_to_string() const {
+	std::stringstream ss;
+	ss << "0x" << std::hex;
+	ss << std::setfill('0') << std::setw(sizeof (UBIGGEST_TYPE)*2);
+	ss << number[0];
+
+	for (unsigned int i = 1; i < number.size(); i++) {
+	    ss << " " << std::setfill('0') << std::setw(sizeof (UBIGGEST_TYPE)*2) << number[i];
 	}
+	return ss.str();
+    }
+    
+    std::ostream &operator<<(std::ostream &out, const UnsignedBigint &a) {
+	out << "0x" << std::hex;
+	out << a.number[0];
 
-	UnsignedBigint::UnsignedBigint(const std::string &representation, unsigned int base) {
-		init_with_base(representation, base);
+	out << std::setfill('0') << std::setw(sizeof (UBIGGEST_TYPE)*2);
+	for (unsigned int i = 1; i < a.number.size(); i++) {
+	    out << std::setfill('0') << std::setw(sizeof (UBIGGEST_TYPE)*2) << a.number[i];
 	}
+	return out;
+    }
 
-	UnsignedBigint::UnsignedBigint(unsigned char* bytes, unsigned int length) {
-		
-	}
-
-	void UnsignedBigint::init_with_base(const std::string& representation, unsigned int base) {
-		//TODO decide how representation should be
-		
-		number.push_back(0);
-	}
-
-	std::string UnsignedBigint::to_string() const {
-		std::stringstream ss;
-		ss << "0x" << std::hex;
-		ss << number[0];
-		ss << std::setfill('0') << std::setw(sizeof (UBIGGEST_TYPE)*2);
-
-		for (unsigned int i = 1; i < number.size(); i++) {
-			ss << number[i];
-		}
-		return ss.str();
-	}
-
-	std::ostream &operator<<(std::ostream &out, const UnsignedBigint &a) {
-		out << "0x" << std::hex;
-		out << a.number[0];
-		out << std::setfill('0') << std::setw(sizeof (UBIGGEST_TYPE)*2);
-		for (unsigned int i = 1; i < a.number.size(); i++) {
-			out << a.number[i];
-		}
-		return out;
-	}
-
-	std::istream &operator>>(std::istream &in, UnsignedBigint &a) {
-		std::string s;
-		in >> s;
-		a = s;
-		return in;
-	}
+    std::istream &operator>>(std::istream &in, UnsignedBigint &a) {
+	std::string s;
+	in >> s;
+	a = s;
+	return in;
+    }
 }
 
